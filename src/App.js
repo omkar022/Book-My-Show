221bab0963df30400c1214977f32810aee7f16a9
omkar022 <omkarjadhav0199@gmail.com>
2022-10-20 15:04:15 +0530

    Initial SetUp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>);
+}
+
+export default App;
